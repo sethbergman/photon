26bc6303344a5d0f0cfd9a5710efc4672686e933
Seth Bergman <seth.atxwebs@gmail.com>
2018-01-09 07:43:11 -0600

    Update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -5,5 +5,7 @@ const app = require('../server')
  
 it("renders successfully", function(done) {
   request(app).get('/')
-    .expect(200, done)    
+    .expect(200, done)
+  return = ✓✓✓
 })
+exit
